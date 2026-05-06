8b1ce967755a31cae99a0f15b098a1d75b48a6b3
mizanmahi <mizanmahi24@gmail.com>
2026-05-06 11:45:54 +0600

    add: implement file upload with WaitGroup and channel for concurrency

--- concurrency/main.go
+++ concurrency/main.go
@@ -8,6 +8,8 @@ import (
 
 var wg sync.WaitGroup
 
+var fileUrl string
+
 func main() {
 
 	var start = time.Now()
@@ -26,18 +28,22 @@ func main() {
 
 	wg.Wait() // waiting .... until counter is 0
 
+	fmt.Println("file url", fileUrl)
 	fmt.Println("all tasks completed")
 	fmt.Println("time taken", time.Since(start))
 }
 
 func uploadFile() {
 	// defer wg.Done()
-	fmt.Println("uploading file...")
-	time.Sleep(3 * time.Second)
-	fmt.Println("File upload done!")
 
+	fmt.Println("uploading file...")
+	time.Sleep(3 * time.Second) // simulating file upload time
+	fmt.Println("File upload done!")
+	fileUrl = "https://s3/sdfdsf.png"
+	// return uploadUrl
 	// wg.Add(-1) // 2
 }
+
 func saveToDB() {
 	// defer wg.Done()
 	fmt.Println("saving to db...")
